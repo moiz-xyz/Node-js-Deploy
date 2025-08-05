bab2723d8474ec8ac5324d6d0c2ee24b079fdba3
moiz-xyz <a.moizniaxi123@gmail.com>
2025-08-05 11:54:31 +0500

    added post api for checking deploymnet

--- index.js
+++ index.js
@@ -1,29 +1,14 @@
 import express from "express";
 import cors from "cors";
 import userRoutes from "./routes/index.js";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = 4000;
 
-// Middlewares
 app.use(express.json());
 app.use(cors());
 
-// 🔥 Register API before static frontend
 app.use("/api", userRoutes);
-
-// Serve frontend build (after API)
-app.use(express.static(path.join(__dirname, "./web/dist")));
-
-// Fallback to index.html for SPA
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./web/dist/index.html"));
-});
 
 // Start server
 app.listen(PORT, () => {
